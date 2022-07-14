91baebb8ac94b46831b381c3a182c5563fb103d4
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-15 00:38:38 +0300

    add gallery markup and hendle click

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Add gallery
+const galleryEl = document.querySelector('.gallery')
+const galleryMarkup = createGalleryMarkup(galleryItems)
+
+galleryEl.innerHTML = galleryMarkup
+
+galleryEl.addEventListener('click', onGalleryItemClick)
+
+
+function createGalleryMarkup (gallery){
+return gallery.map(({preview, original, description}) => 
+`        
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div> `).join('')
+}
+
+function onGalleryItemClick(evt){
+    evt.preventDefault()
+    const largeImgRef = evt.target.dataset.source
+
+    console.log(largeImgRef);
+}
