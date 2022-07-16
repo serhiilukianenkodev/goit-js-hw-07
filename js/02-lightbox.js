fb2b527f928bc6f99f69b54a5b52d32b9a0bcf75
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-16 23:57:02 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery')
 const galleryMarkup = createGalleryMarkup(galleryItems)
 
 galleryEl.innerHTML = galleryMarkup
+
+let gallery = new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionSelector: 'img',
+	captionType: 'attr',
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+	});
 
 function createGalleryMarkup (gallery){
 return gallery.map(({preview, original, description}) => 
@@ -15,15 +24,3 @@ return gallery.map(({preview, original, description}) =>
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a> `).join('')
 }
-
-let gallery = new SimpleLightbox('.gallery a');
-
-gallery.on('show.simplelightbox', function () {
-	// do something…
-});
-
-console.log(gallery);
-
-gallery.on('error.simplelightbox', function (e) {
-	console.log(e); // some usefull information
-});
